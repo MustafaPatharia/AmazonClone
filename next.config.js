9b9f4a82c9f56b0a9fe4384a7a63426781c985eb
preparedProject <patharia52@gmail.com>
2022-06-01 15:44:06 +0400

    Nextjs config updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     images : {
         domains : ["links.papareact.com","fakestoreapi.com","lh3.googleusercontent.com"]
+    },
+    future : {
+        webpack5: true
     }
     
 }
